28f60f0ac2966ff8029bf93ff1f837b685c30d64
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2020-08-30 17:46:12 +0200

    Load Wikipedia List data and show as popup content

--- src/app.js
+++ src/app.js
@@ -1,13 +1,31 @@
 require('leaflet')
 require('leaflet.markercluster')
+let WikipediaListExtractor = require('wikipedia-list-extractor')
+
+let extractor
 
 function showPopup(values) {
   let id = values[1]
   let wikidataId = values[0].match(/http:\/\/www.wikidata.org\/entity\/(.*)$/)[1]
 
-  let ret = '<div class="header"><b>#' + id + '</b> (<a target="_blank" href="https://www.wikidata.org/wiki/' + wikidataId + '">' + wikidataId + '</a>)</div>'
+  let dom = document.createElement('div')
 
-  return ret
+  dom.innerHTML = '<div class="header"><b>#' + id + '</b> (<a target="_blank" href="https://www.wikidata.org/wiki/' + wikidataId + '">' + wikidataId + '</a>)</div>'
+
+  if (extractor) {
+    extractor.get(id, (err, result) => {
+      if (result.length) {
+        let entry = result[0]
+
+        dom.innerHTML +=
+          '<h3>' + (entry.data.title || '')+ '</h3>' +
+          '<p>' + (entry.data.description || '') + '</p>' +
+          '<a target="_blank" href="' + entry.url + '">Quelle</a>'
+      }
+    })
+  }
+
+  return dom
 }
 
 window.onload = function () {
@@ -45,5 +63,13 @@ window.onload = function () {
       })
 
       map.addLayer(markers)
+    })
+
+  global.fetch('node_modules/wikipedia-list-extractor/data/AT-BDA.json')
+    .then(res => res.json())
+    .then(data => {
+      extractor = new WikipediaListExtractor('AT-BDA', data, {
+        proxy: 'proxy/?'
+      })
     })
 }
